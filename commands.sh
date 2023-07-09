d7b68e6fc4d2a86d291eacefa2eeadc1a1793349
Emma <77967471+draumaz@users.noreply.github.com>
2023-07-08 21:00:05 -0700

    work

---
+++ commands.sh
@@ -0,0 +1,9 @@
+FILE="$PWD/oney_doesnt_play.csv"
+
+for LINE in `cat ${FILE} | tr ';' '\n'`; do
+  case $LINE in 0*|1*|2*|3*|4*|5*|6*|7*|8*|9*)
+    FIXED_LINE=`echo ${LINE} | sed s/,/:/`
+    echo "line '${LINE}' -> '${FIXED_LINE}'"
+    sed -i "s/${LINE}/${FIXED_LINE}/g" ${FILE}
+  ;; esac
+done
