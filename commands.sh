0ea6ce8059e29e6b4ad9e9fab16c735a1e2d375c
Emma <77967471+draumaz@users.noreply.github.com>
2023-07-08 21:14:39 -0700

    a

--- commands.sh
+++ commands.sh
@@ -11,12 +11,13 @@ done
 
 sed -i 's/Start Timestamp/Start/g' ${FILE}
 sed -i 's/End Timestamp/End/g' ${FILE}
+sed -i 's/"//g' ${FILE}
 
 # replace ' with "'"
 #sed -i "s/'/\"'\"/g" ${FILE}
 
 # replace , with ","
-sed -i 's/,/","/g' ${FILE}
+#sed -i 's/,/","/g' ${FILE}
 
 # replace ; with ,
 #sed -i 's/;/,/g' ${FILE}
